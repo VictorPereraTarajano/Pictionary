47bf4e21666deb805c8873fd2516e97c232a46e3
VictorPereraTarajano <viic.laspalmas@gmail.com>
2015-02-24 01:10:18 +0000

    Commit.

--- src/view/ui/frame/impl/swing/LobbyFrame.java
+++ src/view/ui/frame/impl/swing/LobbyFrame.java
@@ -73,7 +73,7 @@ public class LobbyFrame extends JFrame implements view.ui.frame.interfaces.Lobby
             }
 
             @Override
-            public void windowIconified(WindowEvent e) {
+            public void windowIconified(WindowEvent e)  {
 
             }
 
